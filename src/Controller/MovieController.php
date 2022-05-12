5b9776cd653f52526138c31ae6d0fe0f4e77cf52
JosephDeFacto <josip.mareljic@gmail.com>
2022-05-13 00:35:24 +0200

    getting all movies & creating movie pdp

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,15 @@ class MovieController extends AbstractController
     /**
      * @Route("/movie", name="app_movie")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $registry): Response
     {
+
+        $movies = $registry->getRepository(Movie::class)->findAll();
+
+
         return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $movies
         ]);
     }
+    
 }
